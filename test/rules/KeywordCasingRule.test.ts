068d774c948bb9e00d1f5a07ebc58034549b9253
Otto Streifel <ottost@microsoft.com>
2017-12-26 08:29:58 -0800

    setup for multiple test cases

--- test/rules/KeywordCasingRule.test.ts
+++ test/rules/KeywordCasingRule.test.ts
@@ -2,18 +2,20 @@ import * as assert from "assert";
 import { ParsedSqlFile } from "../../src/ParsedSqlFile";
 import { KeywordCasingRule } from "../../src/rules/KeywordCasingRule";
 
-it("KeywordCasingRule", () => {
-    const sql = `
-            select
-                name
-            from
-                products
-            where
-                id = 21
-                and name <> "sample"
-        `;
-    const file = new ParsedSqlFile(sql);
-    const rule = new KeywordCasingRule();
-    const failures = rule.apply(file).map((f) => f.triggerText);
-    assert.deepEqual(["select", "from", "where", "and"], failures);
+describe("KeywordCasingRule", () => {
+    it("select * from a where b", () => {
+        const sql = `
+                select
+                    name
+                from
+                    products
+                where
+                    id = 21
+                    and name <> "sample"
+            `;
+        const file = new ParsedSqlFile(sql);
+        const rule = new KeywordCasingRule();
+        const failures = rule.apply(file).map((f) => f.triggerText);
+        assert.deepEqual(["select", "from", "where", "and"], failures);
+    });
 });
