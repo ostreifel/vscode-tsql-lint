7bba0f988c503626dba89471d9305631033dd467
Otto Streifel <ottost@microsoft.com>
2017-12-27 08:17:53 -0800

    more testing of KeywordWordCasingRule

--- src/rules/KeywordCasingRule.ts
+++ src/rules/KeywordCasingRule.ts
@@ -1,6 +1,6 @@
 import { RuleNode } from "antlr4ts/tree/RuleNode";
 import { TerminalNode } from "antlr4ts/tree/TerminalNode";
-import { ConstantContext, IdContext, TSqlParser } from "../../generated/TSqlParser";
+import { Data_typeContext, TSqlParser } from "../../generated/TSqlParser";
 import { BaseRuleWalker } from "../BaseRuleWalker";
 import { BaseSqlRule } from "../BaseSqlRule";
 import { SqlRuleContext } from "../SqlRuleContext";
@@ -8,6 +8,9 @@ import { SqlRuleContext } from "../SqlRuleContext";
 export class KeywordCasingRule extends BaseSqlRule {
     private static readonly notKeyWords: number[] = [
         TSqlParser.LOCAL_ID,
+        TSqlParser.ID,
+        TSqlParser.DOUBLE_QUOTE_ID,
+        TSqlParser.SQUARE_BRACKET_ID,
         TSqlParser.BINARY,
         TSqlParser.BINARY_BASE64,
         TSqlParser.BINARY_CHECKSUM,
@@ -15,32 +18,36 @@ export class KeywordCasingRule extends BaseSqlRule {
         TSqlParser.STRING,
         TSqlParser.QUOTED_URL,
         TSqlParser.QUOTED_HOST_AND_PORT,
+        TSqlParser.IPV4_ADDR,
+        TSqlParser.IPV6_ADDR,
+        TSqlParser.NAME,
     ];
     constructor() {
         super("KeywordCasing");
     }
     protected applyContext(ctx: SqlRuleContext): void {
+        let isDataType = false;
         new class extends BaseRuleWalker {
             public visitTerminal(node: TerminalNode): void {
                 const symbol = node.symbol;
                 if (
+                    !isDataType &&
                     KeywordCasingRule.notKeyWords.indexOf(symbol.type) >= 0
                 ) {
                     return;
                 }
                 const text = ctx.text(symbol);
                 if (text.toLocaleUpperCase() !== text) {
-                    ctx.addError(symbol.startIndex, symbol.stopIndex, "Keywords must be uppercase");
+                    ctx.addError(symbol.startIndex, symbol.stopIndex, `Keywords must be uppercase ${symbol.type}`);
                 }
             }
             public visitChildren(tree: RuleNode): void {
-                if (tree instanceof ConstantContext) {
-                    return;
-                }
-                if (tree instanceof IdContext) {
-                    return;
+                const prevIsDataType = isDataType;
+                if (tree instanceof Data_typeContext) {
+                    isDataType = true;
                 }
                 super.visitChildren(tree);
+                isDataType = prevIsDataType;
             }
         }().visit(ctx.file.node);
     }
--- test/rules/KeywordCasingRule.test.ts
+++ test/rules/KeywordCasingRule.test.ts
@@ -14,7 +14,7 @@ describe("KeywordCasingRule", () => {
                     products
                 where
                     id = 21
-                    and name <> "sample"
+                    and name <> 'sample'
             `;
         const failures = executeRules(sql, rules).map((f) => f.triggerText);
         assert.deepEqual(["select", "from", "where", "and"], failures);
@@ -27,6 +27,25 @@ describe("KeywordCasingRule", () => {
     it("string", () => {
         const sql = `CREATE LOGIN TestLogin
         WITH PASSWORD = 'SuperSecret52&&', SID = 0x241C11948AEEB749B0D22646DB1A19F2;`;
+        const failures = executeRules(sql, rules).map((f) => f.triggerText);
+        assert.deepEqual([], failures);
+    });
+    it("varchar type", () => {
+        const sql = `
+            CREATE TABLE T1 (
+                column_1 int,
+                column_2 varchar(30),
+                rowguid uniqueidentifier,
+                LineTotal money NOT NULL,
+            );
+        `;
+        const failures = executeRules(sql, rules).map((f) => f.triggerText);
+        assert.deepEqual(["int", "varchar", "rowguid", "uniqueidentifier", "money"], failures);
+    });
+    it("special ids", () => {
+        const sql = `
+            SELECT "quoted", [bracketed], p.Id FROM products p
+        `;
         const failures = executeRules(sql, rules).map((f) => f.triggerText);
         assert.deepEqual([], failures);
     });
