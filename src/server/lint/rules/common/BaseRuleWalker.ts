a3fae3a5d53a538ebc4c80b919f0e5128dd78353
Otto Streifel <ottost@microsoft.com>
2018-01-06 19:14:16 -0800

    move lint to own package

--- src/server/lint/rules/common/BaseRuleWalker.ts
+++
@@ -1,7 +0,0 @@
-import { AbstractParseTreeVisitor } from "antlr4ts/tree/AbstractParseTreeVisitor";
-
-export abstract class BaseRuleWalker extends AbstractParseTreeVisitor<void> {
-    protected defaultResult(): void {
-        return undefined;
-    }
-}
